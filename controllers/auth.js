227625933c2dcd66e0de791d73b3477574b66718
isaac-svg <isaacsakyi009@gmail.com>
2023-03-11 21:53:46 +0000

    fixed BadRequestError

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,11 @@ const bcrypt = require("bcryptjs");
 const BadRequestError = require("../errors/bad-request");
 const UnauthenticatedError = require("../errors/unauthenticated");
 const register = async (req, res) => {
-  // try {
   const user = User({ ...req.body });
   await user.save();
   const token = user.createJWT();
 
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
-  // } catch (e) {
-  //   throw new BadRequestError("User creation failed");
-  // }
 };
 
 const login = async (req, res) => {
